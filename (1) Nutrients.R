e97ab6d3cfbe26bcc0f76ab6978bf738f7771a9c
reillyfoconnor <70960206+reillyoc@users.noreply.github.com>
2025-07-20 18:08:19 -0400

    update

--- (1) Nutrients.R
+++ (1) Nutrients.R
@@ -5,6 +5,54 @@
 
 # Load Pkgs
 library(tidyverse)
+library(cowplot)
 
 # load data
-df_ <- read.csv("../", header = T)
+df_fert <- read.csv("../Connectivity/Data/Nutrients/Global_Fertilizer_Consumption.csv", header = T)
+df_nitr <- read.csv("../Connectivity/Data/Nutrients/Nitrogen to Coastal Zone.csv", header = T)
+df_wetland <- read.csv("../Connectivity/Data/Nutrients/Wetland_Loss.csv", header = T)
+df_dz <- read.csv("../Connectivity/Data/Nutrients/Num_Deadzones.csv", header = T)
+
+
+gg_fert <- ggplot(df_fert, aes(x = Year, y = World_Historic)) +
+  geom_smooth(se = F, color = "black") +
+  labs(x = "Year",
+       y = "Global Fertilizer Consumption (Tonnes/Yr)",
+       title = "Flux") +
+  theme_bw(base_size = 14)
+
+gg_fert
+
+
+gg_nitr <- ggplot(df_nitr, aes(x = Year, y = Nitrogen.flux.Mtons.yr.1)) +
+  geom_smooth(se = F, color = "black") +
+  labs(x = "Year",
+       y = "Nitrogen Flux (Mtons per year)",
+       title = "Flux") +
+  theme_bw(base_size = 14)
+
+gg_nitr
+
+gg_wetland <- ggplot(df_wetland, aes(x = x, y = y)) +
+  geom_smooth(se = F, color = "black") +
+  labs(x = "Year",
+       y = "Global Percent Wetland Loss",
+       title = "Connectivity") +
+  theme_bw(base_size = 14)
+
+gg_wetland
+
+gg_dz <- ggplot(df_dz, aes(x = x, y = y)) +
+  geom_smooth(se = F, color = "black") +
+  labs(x = "Year",
+       y = "Reported Number of Deadzones",
+       title = "Imbalance") +
+  theme_bw(base_size = 14)
+
+gg_dz
+
+gg_nutrient_grid <- plot_grid(gg_nitr, gg_wetland, gg_dz, nrow = 1, align = "hv")
+
+gg_nutrient_grid
+
+
